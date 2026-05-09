52adaca30b7ac624230d70fd89d908c7d084349a
ulvu11 <13593@holbertonstudents.com>
2026-05-09 12:52:20 +0300

    Write a new task about Introduction to Cyber Security

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command | awk '$5 != 0 { if ($6 != 0) print }'
+ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command | grep -v " 0 0 "
