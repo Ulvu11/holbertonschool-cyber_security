942aac78b8592ca8bf002583a62ea27c67cfd1e8
ulvu11 <13593@holbertonstudents.com>
2026-05-16 20:56:01 +0300

    Write a new task about Protocols and Servers

--- linux_security/0x03_protocols_servers/2-harden.sh
+++ linux_security/0x03_protocols_servers/2-harden.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find / -type d -perm -0002 ! -type l 2>/dev/null | tee /dev/tty | xargs -I {} chmod o-w "{}" 2>/dev/null
+find / -type d -perm -0002 ! -type l 2>/dev/null -print -exec chmod o-w {} + 2>/dev/null
