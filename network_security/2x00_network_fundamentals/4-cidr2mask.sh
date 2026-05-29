a579bbf0e21f6bfa55616afea762725b63b17a1e
ulvu11 <13593@holbertonstudents.com>
2026-05-29 13:45:57 +0400

    Network Fundamentals: Architecture & Routing

---
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+n=$1; for i in 1 2 3 4; do if (( n >= 8 )); then printf "255"; (( n -= 8 )); elif (( n <= 0 )); then printf "0"; else printf "%s" "$(( 256 - 2**(8-n) ))"; n=0; fi; if(( i != 4 )); then printf "."; fi; done;
