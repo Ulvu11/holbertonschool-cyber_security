3b58635df4075210c50ed29768f7dfdc1a23b4db
ulvu11 <13593@holbertonstudents.com>
2026-05-29 17:19:53 +0400

    Network Fundamentals: Architecture & Routing

--- network_security/2x00_network_fundamentals/5-network_id.sh
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS="." read -ra list <<< "$1";IFS="." read -r a b c d <<< "$2"; n=0; for i in $a $b $c $d; do if (( i != 0 )); then printf "%s" "${list[$n]}"; else printf "0"; fi; if (( n != 3 )); then printf "."; fi; (( n += 1 )); done 
+IFS="." read -ra ip <<< "$1";IFS="." read -ra mask <<< "$2"; for i in 0 1 2 3 ; do printf "%s" "$(( ${ip[$i]} & ${mask[$i]} ))"; if (( i != 3 )); then printf "."; fi; done
